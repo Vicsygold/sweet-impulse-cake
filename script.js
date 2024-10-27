6bd2378fb91675c405135fd213c0059ef5b93eee
Vicsygold <64321248+Vicsygold@users.noreply.github.com>
2024-10-27 17:05:51 +0100

    new

---
+++ script.js
@@ -0,0 +1,16 @@
+window.addEventListener('scroll', function() {
+    var navbar = document.getElementById('navbar');
+    var scrollPosition = window.scrollY;
+
+    if (scrollPosition > 50) { // Adjust the scroll position value as needed
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+
+
+function toggleMenu() {
+    const links = document.getElementById("links");
+    links.classList.toggle("show");
+}
